4b08dda0f4c69cd3e1e6d2c2fab802ac3067cc23
Pietras1994 <pieter8965@gmail.com>
2017-03-06 10:26:33 +0100

    Add project files.

---
+++ Rest/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Rest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.LoadBtn = new System.Windows.Forms.Button();
+            this.RefreashBtn = new System.Windows.Forms.Button();
+            this.ClearBtn = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Category = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Title = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Link = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Summary = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(698, 134);
+            this.listBox1.TabIndex = 0;
+            // 
+            // LoadBtn
+            // 
+            this.LoadBtn.Location = new System.Drawing.Point(716, 12);
+            this.LoadBtn.Name = "LoadBtn";
+            this.LoadBtn.Size = new System.Drawing.Size(94, 39);
+            this.LoadBtn.TabIndex = 1;
+            this.LoadBtn.Text = "Wczytaj";
+            this.LoadBtn.UseVisualStyleBackColor = true;
+            // 
+            // RefreashBtn
+            // 
+            this.RefreashBtn.Location = new System.Drawing.Point(716, 57);
+            this.RefreashBtn.Name = "RefreashBtn";
+            this.RefreashBtn.Size = new System.Drawing.Size(94, 39);
+            this.RefreashBtn.TabIndex = 2;
+            this.RefreashBtn.Text = "Odswiez";
+            this.RefreashBtn.UseVisualStyleBackColor = true;
+            // 
+            // ClearBtn
+            // 
+            this.ClearBtn.Location = new System.Drawing.Point(716, 102);
+            this.ClearBtn.Name = "ClearBtn";
+            this.ClearBtn.Size = new System.Drawing.Size(94, 39);
+            this.ClearBtn.TabIndex = 3;
+            this.ClearBtn.Text = "Wyczysc";
+            this.ClearBtn.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Category,
+            this.Title,
+            this.Link,
+            this.Summary});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 152);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(798, 203);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // Category
+            // 
+            this.Category.HeaderText = "Kategoria";
+            this.Category.Name = "Category";
+            // 
+            // Title
+            // 
+            this.Title.HeaderText = "Tytuł";
+            this.Title.Name = "Title";
+            // 
+            // Link
+            // 
+            this.Link.HeaderText = "Link";
+            this.Link.Name = "Link";
+            // 
+            // Summary
+            // 
+            this.Summary.HeaderText = "Podsumowanie";
+            this.Summary.Name = "Summary";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(822, 476);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.ClearBtn);
+            this.Controls.Add(this.RefreashBtn);
+            this.Controls.Add(this.LoadBtn);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button LoadBtn;
+        private System.Windows.Forms.Button RefreashBtn;
+        private System.Windows.Forms.Button ClearBtn;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Category;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Title;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Link;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Summary;
+    }
+}
+
